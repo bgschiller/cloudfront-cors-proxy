aff5bf46d63dd13580897e98b81444c5d1745dc5
Brian Schiller <bgschiller@gmail.com>
2021-10-01 14:39:33 -0700

    Fill out body of the cdk stack

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
-# Welcome to your CDK TypeScript project!
+# Cloudfront CORS Proxy
 
-This is a blank project for TypeScript development with CDK.
+Quickly set up a proxy to add permissive CORS headers to an API you'd like to access.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+Learn more at (blog post url incoming)
+
+## Quick Start
+
+1. Clone down this repo and edit bin/cloudfront-cors-proxy.ts to point to the API origin you want to proxy to.
+2. `npm install`
+3. Make sure you have aws access key and secret keys available. An easy way to check is to run `aws configure list`.
+3. Bootstrap `npm run cdk bootstrap` (I don't know what this is for)
+4. Deploy `npm run cdk -- deploy --all`
 
 ## Useful commands
 
  * `npm run build`   compile typescript to js
  * `npm run watch`   watch for changes and compile
  * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `npm run -- cdk deploy --all`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
--- lib/cloudfront-cors-proxy-stack.ts
+++ lib/cloudfront-cors-proxy-stack.ts
@@ -1,9 +1,66 @@
-import * as cdk from '@aws-cdk/core';
+import { HttpHeaders } from '@cloudcomponents/cdk-lambda-at-edge-pattern';
+import { AllowedMethods, CacheCookieBehavior, CacheHeaderBehavior, CachePolicy, CacheQueryStringBehavior, Distribution, PriceClass, } from '@aws-cdk/aws-cloudfront';
+import { CfnOutput, Construct, Duration, Stack, StackProps } from '@aws-cdk/core';
+import { HttpOrigin } from '@aws-cdk/aws-cloudfront-origins';
 
-export class CloudfrontCorsProxyStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export interface CloudfrontCorsProxyProps extends StackProps {
+  originToHit: string;
+  extraRequestHeaders?: string[]; // always includes Authorization, Origin, X-Api-Key
+  withCredentials?: boolean; // defaults to false
+  allowedOrigin?: string; // defaults to '*'
+  allowedMethods?: string; // defaults to '*'
+  allowedHeaders?: string; // defaults to '*'
+}
+
+export class CloudfrontCorsProxyStack extends Stack {
+  constructor(scope: Construct, id: string, props: CloudfrontCorsProxyProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const  {
+      originToHit,
+      extraRequestHeaders = [],
+      allowedOrigin = '*',
+      allowedHeaders = '*',
+      allowedMethods = '*',
+      withCredentials = false
+    } = props;
+    if (withCredentials && (allowedOrigin === '*' || allowedMethods === '*' || allowedHeaders === '*')) {
+      throw new Error('CORS forbids including credentials with a wildcard origin, headers, or methods list');
+    }
+    const headers = {
+      'Access-Control-Allow-Origin': allowedOrigin,
+      'Access-Control-Allow-Credentials': `${withCredentials}`,
+      'Access-Control-Allow-Headers': allowedHeaders,
+      'Access-Control-Allow-Methods': allowedMethods,
+    };
+
+    const httpHeaders = new HttpHeaders(this, 'RewriteHeaders', {
+      httpHeaders: headers
+    });
+    const distribution = new Distribution(this, 'Distribution', {
+      priceClass: PriceClass.PRICE_CLASS_200,
+      defaultBehavior: {
+        origin: new HttpOrigin(originToHit),
+        allowedMethods: AllowedMethods.ALLOW_ALL,
+        cachePolicy: new CachePolicy(this, 'CachePolicy', {
+          cookieBehavior: CacheCookieBehavior.all(),
+          defaultTtl: Duration.seconds(0),
+          headerBehavior: CacheHeaderBehavior.allowList('Authorization', 'Origin', 'X-Api-Key', ...extraRequestHeaders),
+          queryStringBehavior: CacheQueryStringBehavior.all(),
+        }),
+        edgeLambdas: [httpHeaders],
+      }
+    });
+
+    new CfnOutput(this, 'distribution id', {
+      value: distribution.distributionId,
+    });
+    new CfnOutput(this, 'domain name', {
+      value: distribution.distributionDomainName,
+    });
+    new CfnOutput(this, 'message', {
+      value: `Access ${originToHit} without CORS restrictions using ${distribution.distributionDomainName} as a proxy`,
+    });
+
   }
 }
